e12c7325b962c260b7b6f03022ac7a4a080fa5f6
Matthew Zember <matthew.zember@jupiterone.com>
2022-05-18 13:15:40 -0400

    Removing dead comment

--- src/ms-graph/utils.ts
+++ src/ms-graph/utils.ts
@@ -9,8 +9,6 @@ export function isRetryable(err: any, retries: number): boolean {
   // Others we could consider adding if necessary: 408, 503 and others
   const retryableStatusCodes = [-1, 408, 500, 502];
 
-  // A FetchError is a connection timeout that we should retry
-  // retryable can be type boolean, "", or undefined
   return (
     retries < 5 &&
     (retryableErrorMessages.includes(err.message) ||
