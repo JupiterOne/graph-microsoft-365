dd0b1d6a44a16a7c3a14d9209256a6d6353808e2
Michael Knoedel <mknoedel@gmail.com>
2024-01-03 11:20:46 -0800

    add deviceId

--- src/steps/intune/steps/devices/converters.ts
+++ src/steps/intune/steps/devices/converters.ts
@@ -146,7 +146,9 @@ export function createManagedDeviceEntity(
         username: managedDevice.userPrincipalName,
         physical: isPhysicalDevice,
         // Used to map HOSTAGENT to user_endpoint via user_endpoint's udid
-        deviceId: null,
+        deviceId:
+          managedDevice.azureActiveDirectoryDeviceId ??
+          managedDevice.azureADDeviceId, // I believe these are legacy values now
         // POTENTIAL: managedDevice.usersLoggedOn - link out to other users perhaps?
         deviceCategoryDisplayName: managedDevice.deviceCategoryDisplayName,
         lastSeenOn: parseTimePropertyValue(managedDevice.lastSyncDateTime),
