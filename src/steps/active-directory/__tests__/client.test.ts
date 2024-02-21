fc0816f10e1210bc05f97fe42fd4bd42e4a07186
Gonzalo Avalos Ribas <gonzaloavalosribas@Gonzalos-MacBook-Pro.local>
2024-02-21 10:00:43 -0300

    Corrected test

--- src/steps/active-directory/__tests__/client.test.ts
+++ src/steps/active-directory/__tests__/client.test.ts
@@ -65,11 +65,11 @@ describe('iterateUsers', () => {
       });
 
     const resources: User[] = [];
-    await client.iterateUsers((e) => {
-      resources.push(e);
-    });
-
-    expect(resources.length).toEqual(0);
+    await expect(async () => {
+      await client.iterateUsers((e) => {
+        resources.push(e);
+      });
+    }).rejects.toThrow();
   });
 
   // TODO @zemberdotnet INT-5328 Reenable/rerecord test
